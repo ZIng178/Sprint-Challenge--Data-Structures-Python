8b7391fe6a29aef725660892184537cbd74c9ab1
Wangdi Tenzing <wangdi457@gmail.com>
2020-09-27 21:36:38 -0400

    ringbuffer complete with passing tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,27 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity=capacity #size
+        self.ringbuffer=[] #empty
+        self.current_index=0 #current index
 
     def append(self, item):
-        pass
+        #compare the length of the empty and the capacity 
+        # if the list is not full capacity append the item to the end 
+        if len(self.ringbuffer)<self.capacity:
+            self.ringbuffer.append(item)
+       
+       
+        # if the list is full capacity, remove the oldest and insert the new item in its place 
+        
+        else:
+            self.ringbuffer[self.current_index]=item
+            self.current_index +=1
+        
+        if  self.current_index == self.capacity:
+            self.current_index=0
+    
+
 
     def get(self):
-        pass
+        #return the list 
+        return self.ringbuffer
